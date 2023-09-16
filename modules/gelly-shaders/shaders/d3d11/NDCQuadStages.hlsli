3da1aa1ed520d4e1cdde25921037511e8c2216fb
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-09-16 15:01:39 -0700

    Add OutputEncoder and new NDCQuad include file

---
+++ modules/gelly-shaders/shaders/d3d11/NDCQuadStages.hlsli
@@ -0,0 +1,9 @@
+struct VS_INPUT {
+	float4 Position : SV_Position;
+	float2 Texcoord : TEXCOORD;
+};
+
+struct VS_OUTPUT {
+	float4 Position : SV_Position;
+	float2 Texcoord : TEXCOORD;
+};
---
+++ modules/gelly-shaders/shaders/d3d11/OutputEncoder.ps.hlsl
@@ -0,0 +1,24 @@
+#include "NDCQuadStages.hlsli"
+#include "SplitFloat.hlsli"
+
+struct PS_OUTPUT {
+    float4 EncodedDepth : SV_TARGET0;
+    float4 EncodedNormal : SV_TARGET1;
+};
+
+// The "encoding" is just converting the internal 32-bit float textures to the 16-bit float textures
+
+Texture2D InternalDepth : register(t0);
+Texture2D InternalNormal : register(t1);
+SamplerState InternalDepthSampler : register(s0);
+SamplerState InternalNormalSampler : register(s1);
+
+PS_OUTPUT main(VS_OUTPUT input) {
+    PS_OUTPUT output;
+
+    float depth = InternalDepth.Sample(InternalDepthSampler, input.Texcoord).r;
+    output.EncodedDepth = float4(SplitFloat(depth), 0, 0);
+    output.EncodedNormal = InternalNormal.Sample(InternalNormalSampler, input.Texcoord);
+
+    return output;
+}
