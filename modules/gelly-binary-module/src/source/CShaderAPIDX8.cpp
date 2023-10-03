b28f4414c02fda103da48b25fe58542f68622f43
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-10-03 15:02:18 -0700

    Remove unused and test code from `CShaderAPIDX8.cpp`

--- modules/gelly-binary-module/src/source/CShaderAPIDX8.cpp
+++ modules/gelly-binary-module/src/source/CShaderAPIDX8.cpp
@@ -1,59 +1,24 @@
 #include "CShaderAPIDX8.h"
 
 #include <cassert>
-#include <cstdio>
 
 #include "Interface.h"
 #include "hooking/Library.h"
 #include "hooking/VTable.h"
 
 static CShaderAPIDX8 shaderAPIDX8 = nullptr;
-static VTable shaderAPIVTable;
-
-static const uintptr_t SetAmbientLightCubeIndex = 130;
-static const uintptr_t GetD3DTextureIndex = 264;
-
 static Library shaderAPILib;
 
 static const char *GetD3DTextureSignature =
 	"48 85 d2 75 03 33 c0 c3 80 7a 1a 01 48 8b";
 
-static const char *IsTextureSignature =
-	"48 85 d2 74 2e 48 39 52 70 75 06 48 39 52";
-
 typedef IDirect3DBaseTexture9 *(__thiscall *GetD3DTextureFn)(
 	CShaderAPIDX8, unsigned long long
 );
 
-typedef unsigned long long(__thiscall *IsTextureFn)(
-	CShaderAPIDX8, void *, ShaderAPITextureHandle_t
-);
-
-// static HookedFunction IsTextureHook;
-// static IsTextureFn IsTextureHookFnOrig;
-//
-// unsigned long long __thiscall IsTextureHookFn(
-//	CShaderAPIDX8 thisPtr, void *edx, ShaderAPITextureHandle_t handle
-//) {
-//	printf("IsTextureHookFn\n");
-//	printf("thisPtr: %p\n", thisPtr);
-//	printf("handle: %p\n", handle);
-//
-//	return IsTextureHookFnOrig(thisPtr, edx, handle);
-// }
-
 CShaderAPIDX8 GetShaderAPIDX8() {
 	if (!shaderAPILib.IsInitialized()) {
 		shaderAPILib.Init("shaderapidx9.dll");
-
-		//		shaderAPILib.HookFunction(
-		//			IsTextureSignature,
-		//			reinterpret_cast<void *>(IsTextureHookFn),
-		//			reinterpret_cast<void **>(&IsTextureHookFnOrig),
-		//			IsTextureHook
-		//		);
-		//
-		//		IsTextureHook.Enable();
 	}
 
 	if (shaderAPIDX8 == nullptr) {
@@ -64,8 +29,6 @@ CShaderAPIDX8 GetShaderAPIDX8() {
 #ifdef _DEBUG
 		assert(shaderAPIDX8 != nullptr);
 #endif
-
-		shaderAPIVTable.Init(*shaderAPIDX8, 266);
 	}
 
 	return shaderAPIDX8;
@@ -77,18 +40,8 @@ IDirect3DBaseTexture9 *GetD3DTexture(CTexture *texture) {
 	auto getD3DTexture =
 		shaderAPILib.FindFunction<GetD3DTextureFn>(GetD3DTextureSignature);
 
-	//	auto isTexture =
-	// shaderAPILib.FindFunction<IsTextureFn>(IsTextureSignature);
-	//
-	//	unsigned long long test = isTexture(shaderAPIDX8, handle);
-	//
-	//	printf("thisPtr: %p\n", shaderAPIDX8);
-	//	printf("test: %p\n", test);
-
 	IDirect3DBaseTexture9 *d3dTexture =
 		getD3DTexture(GetShaderAPIDX8(), (unsigned long long)handle);
-
-	printf("d3dTexture: %p\n", d3dTexture);
 
 	return d3dTexture;
 }
