78500a8c4c3a33e06618e0958f0253b86770ecad
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-10-05 13:53:44 -0700

    Add class skeleton to GellyEngineGMod.h

--- modules/gelly-binary-module/src/engine/GellyEngineGMod.h
+++ modules/gelly-binary-module/src/engine/GellyEngineGMod.h
@@ -1,6 +1,23 @@
 #ifndef GELLY_GELLYENGINEGMOD_H
 #define GELLY_GELLYENGINEGMOD_H
 
-class GellyEngineGMod {};
+#include <GellyEngine.h>
+
+class GellyEngineGMod {
+private:
+	GellyScene *scene;
+
+	MeshUploadInfo ProcessBSP(uint8_t *data, size_t dataSize);
+
+public:
+	GellyEngineGMod(
+		int maxParticles, int maxColliders, void *d3d11Device = nullptr
+	);
+	~GellyEngineGMod();
+
+	GellyScene *GetScene();
+
+	void AddBSP(const std::string &mapName, uint8_t *data, size_t dataSize);
+};
 
 #endif	// GELLY_GELLYENGINEGMOD_H
