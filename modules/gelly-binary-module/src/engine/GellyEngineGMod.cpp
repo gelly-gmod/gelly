d566bc44ffe3aaf57ea87aa4836264261055265f
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-10-05 14:01:02 -0700

    Add constructor to GellyEngineGMod.cpp

--- modules/gelly-binary-module/src/engine/GellyEngineGMod.cpp
+++ modules/gelly-binary-module/src/engine/GellyEngineGMod.cpp
@@ -1,1 +1,18 @@
 #include "GellyEngineGMod.h"
+
+#include <cassert>
+
+GellyEngineGMod::GellyEngineGMod(
+	int maxParticles, int maxColliders, void *d3d11Device
+)
+	: scene(nullptr) {
+	scene = GellyEngine_CreateScene(maxParticles, maxColliders, d3d11Device);
+
+	// This is really a completely unrecoverable error, and this should exit the
+	// app while giving a reasonably detailed explanation to the user, but I
+	// haven't standardized this across modules. For the time being, this should
+	// do.
+#ifdef _DEBUG
+	assert(scene != nullptr);
+#endif
+}
