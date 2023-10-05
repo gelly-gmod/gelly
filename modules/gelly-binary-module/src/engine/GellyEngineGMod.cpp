d8bb6ca5ce06ecc255ae7c24275d47cf2b1c6b9d
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-10-05 15:09:49 -0700

    Copy over MeshConvert_LoadBSP to GellyEngineGMod.cpp

--- modules/gelly-binary-module/src/engine/GellyEngineGMod.cpp
+++ modules/gelly-binary-module/src/engine/GellyEngineGMod.cpp
@@ -2,6 +2,8 @@
 
 #include <cassert>
 
+#include "BSPParser.h"
+
 GellyEngineGMod::GellyEngineGMod(
 	int maxParticles, int maxColliders, void *d3d11Device
 )
@@ -26,3 +28,36 @@ GellyEngineGMod::~GellyEngineGMod() {
 }
 
 GellyScene *GellyEngineGMod::GetScene() { return scene; }
+
+MeshUploadInfo GellyEngineGMod::ProcessBSP(uint8_t *data, size_t dataSize) {
+	BSPMap parsedMap(data, dataSize);  // FleX considers CCW triangles to be
+									   // front-facing, so we need to flip them
+	if (!parsedMap.IsValid()) {
+		throw std::runtime_error("Invalid map data");
+	}
+
+	MeshUploadInfo info{};
+	info.vertexCount = static_cast<int>(parsedMap.GetNumTris()) * 3;
+	info.indexCount = info.vertexCount;
+
+	info.vertices = new Vec3[info.vertexCount];
+	info.indices = new int[info.indexCount];
+
+	memcpy(
+		info.vertices, parsedMap.GetVertices(), sizeof(Vec3) * info.vertexCount
+	);
+
+	for (int i = 0; i < info.indexCount; i += 3) {
+		info.indices[i] = i + 2;
+		info.indices[i + 1] = i + 1;
+		info.indices[i + 2] = i;
+	}
+
+	Vec3 upper = Vec3{16384, 16384, 16384};
+	Vec3 lower = Vec3{-16384, -16384, -16384};
+
+	info.upper = upper;
+	info.lower = lower;
+
+	return info;
+}
--- modules/gelly-binary-module/src/engine/GellyEngineGMod.h
+++ modules/gelly-binary-module/src/engine/GellyEngineGMod.h
@@ -7,13 +7,14 @@ class GellyEngineGMod {
 private:
 	GellyScene *scene;
 
-	MeshUploadInfo ProcessBSP(uint8_t *data, size_t dataSize);
+	static MeshUploadInfo ProcessBSP(uint8_t *data, size_t dataSize);
+	static void FreeBSP(MeshUploadInfo &info);
 
 public:
 	GellyEngineGMod(
 		int maxParticles, int maxColliders, void *d3d11Device = nullptr
 	);
-	
+
 	~GellyEngineGMod();
 
 	GellyScene *GetScene();
