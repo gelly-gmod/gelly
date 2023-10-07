01979193de27a824a870cfbaa71d189c4fd7c67e
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-10-06 17:55:57 -0700

    Finish IsosurfaceExtraction's declaration

--- modules/gelly-renderer/src/rendering/techniques/IsosurfaceExtraction.h
+++ modules/gelly-renderer/src/rendering/techniques/IsosurfaceExtraction.h
@@ -6,12 +6,19 @@
 
 #include "rendering/Technique.h"
 
+using IntBuffer = d3d11::Buffer<int>;
+using IntUAV = d3d11::UAVBuffer<int>;
+
 class IsosurfaceExtraction : public Technique {
 private:
 	d3d11::ComputeProgram extractionProgram;
-	d3d11::Buffer<int> neighborBuffer;
-	d3d11::Buffer<int> remapBuffer;
-	d3d11::Buffer<int> neighborCountBuffer;
+	IntBuffer neighborBuffer;
+	IntBuffer remapBuffer;
+	IntBuffer neighborCountBuffer;
+	IntUAV neighborUAV;
+	IntUAV remapUAV;
+	IntUAV neighborCountUAV;
+	d3d11::ComputeProgramLayout<PerFrameCBuffer> layout;
 
 public:
 	IsosurfaceExtraction(ID3D11Device *device, int maxParticles);
