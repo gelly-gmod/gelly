af68f66a5b6f4b64b8c3331c6d7bdffe112b8048
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-10-06 17:20:43 -0700

    Add `IsosurfaceExtraction`

---
+++ modules/gelly-renderer/src/rendering/techniques/IsosurfaceExtraction.h
@@ -0,0 +1,10 @@
+#ifndef GELLY_ISOSURFACEEXTRACTION_H
+#define GELLY_ISOSURFACEEXTRACTION_H
+
+#include <GellyD3D.h>
+#include <d3d11.h>
+
+#include "rendering/Technique.h"
+
+class IsosurfaceExtraction : public Technique {};
+#endif	// GELLY_ISOSURFACEEXTRACTION_H
