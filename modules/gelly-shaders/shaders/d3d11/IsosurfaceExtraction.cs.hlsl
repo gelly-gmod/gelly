c047d9a2704722d1881ff8eba9a212242f571735
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-10-07 20:28:13 -0700

    Working on isosurface extraction, primarily normal estimation on the density field.

--- modules/gelly-shaders/shaders/d3d11/IsosurfaceExtraction.cs.hlsl
+++ modules/gelly-shaders/shaders/d3d11/IsosurfaceExtraction.cs.hlsl
@@ -18,16 +18,57 @@ uint GetNeighborCount(uint index) {
     return neighborCounts[internalIndex];
 }
 
-float3 GetColorForIndex(uint index) {
-    uint r = (index + 22) % 255;
-    uint g = (index + 32) % 255;
-    uint b = (index + 2) % 255;
+float3 GetWorldPos(uint x, uint y, uint w, uint h, float depth) {
+    // We have to convert window space coords to clip space coords.
+    float u = (float)x / (float)w;
+    float v = (float)y / (float)h;
 
-    return float3(
-        (float)r,
-        (float)g,
-        (float)b
-    ) / float3(255, 255, 255);
+    float clipX = u * 2.f - 1.0;
+    float clipY = (1.f - v) * 2.f - 1.0;
+    float clipZ = depth;
+
+    float4 clipCoord = float4(clipX, clipY, clipZ, 1.f);
+    float4 viewPosition = mul(clipCoord, matInvProj);
+    float4 worldPosition = mul(viewPosition / viewPosition.w, matInvView);
+
+    return worldPosition.xyz;
+}
+
+float SmoothingKernel(float3 worldPos, float3 neighborPos, float radius) {
+    return 1.f - pow(length(normalize(worldPos - neighborPos)) / radius, 3);
+}
+
+float GetDensity(float3 worldPos, uint index) {
+    float sum = 0.f;
+
+    // Iterate over each neighbor and apply the smoothing function.
+    /*
+    // find offset in the neighbors buffer
+		int offset = apiToInternal[i];
+		int count = counts[offset];
+
+		for (int c=0; c  count; ++c)
+		{
+			int neighbor = internalToApi[neighbors[c*stride + offset]];
+
+			printf("Particle %d's neighbor %d is particle %d\n", i, c, neighbor);
+		}
+        */
+
+    uint stride = 2100001;
+    uint offset = apiToInternal[index];
+    uint count = neighborCounts[offset];
+
+    for (int c = 0; c < count; c++) {
+        int neighbor = internalToAPI[neighborIndices[c*stride + offset]];
+        float3 neighborPos = positions[neighbor].xyz;
+
+        float m_j = 0.05f;
+        float r = 5.f;
+        sum += m_j * SmoothingKernel(worldPos, neighborPos, r);
+    }
+
+    return sum;
 }
 
 // The isosurface reconstruction is done in 4x4 tiles.
@@ -43,10 +84,24 @@ void main(uint3 id : SV_DispatchThreadID) {
     }
 
     float2 depthIndex = depth[id.xy].xy;
-
     float depthValue = depthIndex.x;
+    if (depthValue == 1.f) {
+        return;
+    }
+
     uint index = asuint(depthIndex.y);
-    uint neighborCount = GetNeighborCount(index);
-    float debugValue = (float)neighborCount / 64.f;
-    normal[id.xy] = float4(debugValue, debugValue, debugValue, 1.f);
+    
+    float3 worldPos = GetWorldPos(id.x, id.y, width, height, depthValue);
+    float isoValue = 5.f;
+    float density = GetDensity(worldPos, index) + 0.001f;
+
+    // Calculate the normal by taking the gradient of the density field.
+    float3 gradient = float3(0, 0, 0);
+    // TODO: THIS IS RETURNING THE SAME VALUE FOR EVERY PIXEL. WHY?
+    float dist = 3345.f;
+    float xDensity = GetDensity(worldPos + float3(dist, 0, 0), index);
+    float yDensity = GetDensity(worldPos + float3(0, dist, 0), index);
+    float zDensity = GetDensity(worldPos + float3(0, 0, dist), index);
+
+    normal[id.xy] = float4(xDensity, yDensity, zDensity, 1.f);
 }
