3142c51786832aefbf84c0e3f0a1eeab69f8dc88
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-10-09 14:36:04 -0700

    Refactor D3DDeviceWrapper.cpp to use the new Library class

--- modules/gelly-binary-module/src/source/D3DDeviceWrapper.cpp
+++ modules/gelly-binary-module/src/source/D3DDeviceWrapper.cpp
@@ -1,37 +1,17 @@
 #include "D3DDeviceWrapper.h"
 
-#include <TlHelp32.h>
 #include <windows.h>
 
-uintptr_t get_shaderapi_base_address() {
-	// Take a snapshot of the GMod process. We want to see what modules are
-	// loaded into the process.
+#include "hooking/Library.h"
 
-	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, 0);
+static Library shaderAPI;
 
-	if (snapshot == INVALID_HANDLE_VALUE) {
-		return 0;
+void EnsureShaderAPILoaded() {
+	if (shaderAPI.IsInitialized()) {
+		return;
 	}
 
-	// Look for the shader API module.
-
-	MODULEENTRY32 module_entry;
-	module_entry.dwSize = sizeof(MODULEENTRY32);
-
-	if (!Module32First(snapshot, &module_entry)) {
-		CloseHandle(snapshot);
-		return 0;
-	}
-
-	do {
-		if (strcmp(module_entry.szModule, "shaderapidx9.dll") == 0) {
-			CloseHandle(snapshot);
-			return (uintptr_t)module_entry.modBaseAddr;
-		}
-	} while (Module32Next(snapshot, &module_entry));
-
-	CloseHandle(snapshot);
-	return 0;
+	shaderAPI.Init("shaderapidx9.dll");
 }
 
 /**
@@ -41,13 +21,10 @@ const uintptr_t D3DDeviceWrapperOffset = 0x9A9E0;
 
 IDirect3DDevice9Ex *GetD3DDevice() {
 	// Grab the BaseShaderAPIDLL module.
-	uintptr_t baseShaderAPIDLL = get_shaderapi_base_address();
-	if (!baseShaderAPIDLL) {
-		return nullptr;
-	}
+	EnsureShaderAPILoaded();
 
 	auto *d3dDeviceWrapper =
-		(D3DDeviceWrapper *)(baseShaderAPIDLL + D3DDeviceWrapperOffset);
+		shaderAPI.GetObjectAt<D3DDeviceWrapper *>(D3DDeviceWrapperOffset);
 
 	if (!d3dDeviceWrapper) {
 		return nullptr;
