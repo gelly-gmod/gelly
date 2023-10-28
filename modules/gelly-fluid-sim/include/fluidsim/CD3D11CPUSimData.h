75ef272bd2e804bca6843c7e9c5b01a2f7e8cbe4
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-10-28 15:52:24 -0700

    Work on ISimData

---
+++ modules/gelly-fluid-sim/include/fluidsim/CD3D11CPUSimData.h
@@ -0,0 +1,7 @@
+#ifndef GELLY_CD3D11CPUSIMDATA_H
+#define GELLY_CD3D11CPUSIMDATA_H
+
+#include <GellyD3D.h>
+#include <d3d11.h>
+
+#endif	// GELLY_CD3D11CPUSIMDATA_H
--- modules/gelly-fluid-sim/include/fluidsim/ISimData.h
+++ modules/gelly-fluid-sim/include/fluidsim/ISimData.h
@@ -3,6 +3,10 @@
 
 #include "GellyInterface.h"
 
+struct SimFloat4 {
+	float x, y, z, w;
+};
+
 enum class SimBuffer {
 	Position,
 	Velocity,
@@ -10,7 +14,12 @@ enum class SimBuffer {
 
 gelly_interface ISimData {
 public:
-	virtual ~ISimData() = default;
+	/**
+	 * Destroys the underlying buffers.
+	 */
+	virtual ~ISimData() = 0;
+
+	virtual void Initialize(int maxParticles) = 0;
 
 	/**
 	 * Returns a buffer created in a rendering API. The exact one is determined
@@ -19,6 +28,9 @@ public:
 	 * @return
 	 */
 	virtual void *GetRenderBuffer(SimBuffer buffer) = 0;
+
+	virtual SimFloat4 *MapBuffer(SimBuffer buffer) = 0;
+	virtual void UnmapBuffer(SimBuffer buffer) = 0;
 };
 
 #endif	// GELLY_ISIMDATA_H
