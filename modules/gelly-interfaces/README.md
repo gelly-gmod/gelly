73b79ef27d218ece3a46241b1cdc1b82384a3d1b
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-10-28 12:50:49 -0700

    Add a standardized module for creating interfaces

---
+++ modules/gelly-interfaces/README.md
@@ -0,0 +1,3 @@
+# gelly-interfaces
+
+Simple header-only module which holds shared macros for defining a Gelly interface.
---
+++ modules/gelly-interfaces/include/GellyInterface.h
@@ -0,0 +1,6 @@
+#ifndef GELLY_GELLYINTERFACE_H
+#define GELLY_GELLYINTERFACE_H
+
+#define gelly_interface __declspec(novtable) class
+
+#endif	// GELLY_GELLYINTERFACE_H
