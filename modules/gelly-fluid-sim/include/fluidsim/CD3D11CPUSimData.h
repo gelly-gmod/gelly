679025c336ef83c39132d8e8841aae01195de160
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-10-28 16:34:20 -0700

    Implement a D3D11 CPU-based subclass of ISimData

--- modules/gelly-fluid-sim/include/fluidsim/CD3D11CPUSimData.h
+++ modules/gelly-fluid-sim/include/fluidsim/CD3D11CPUSimData.h
@@ -4,4 +4,25 @@
 #include <GellyD3D.h>
 #include <d3d11.h>
 
+#include "ISimContext.h"
+#include "ISimData.h"
+
+class CD3D11CPUSimData : ISimData {
+private:
+	d3d11::Buffer<SimFloat4> positions;
+	ID3D11Device *device;
+	ID3D11DeviceContext *deviceContext;
+
+public:
+	explicit CD3D11CPUSimData(ISimContext *context);
+	~CD3D11CPUSimData() override = default;
+
+	void Initialize(int maxParticles) override;
+
+	void *GetRenderBuffer(SimBuffer buffer) override;
+
+	SimFloat4 *MapBuffer(SimBuffer buffer) override;
+	void UnmapBuffer(SimBuffer buffer) override;
+};
+
 #endif	// GELLY_CD3D11CPUSIMDATA_H
---
+++ modules/gelly-fluid-sim/src/fluidsim/CD3D11CPUSimData.cpp
@@ -0,0 +1,78 @@
+#include "fluidsim/CD3D11CPUSimData.h"
+
+#include <cassert>
+
+CD3D11CPUSimData::CD3D11CPUSimData(ISimContext *context)
+	: device(reinterpret_cast<ID3D11Device *>(
+		  context->GetRenderAPIHandle(RenderAPIHandle::D3D11Device)
+	  )),
+	  deviceContext(reinterpret_cast<ID3D11DeviceContext *>(
+		  context->GetRenderAPIHandle(RenderAPIHandle::D3D11DeviceContext)
+	  )) {
+#ifdef _DEBUG
+	assert(device != nullptr);
+	assert(deviceContext != nullptr);
+#endif
+}
+
+void CD3D11CPUSimData::Initialize(int maxParticles) {
+	positions.Init(
+		device,
+		maxParticles,
+		nullptr,
+		static_cast<D3D11_BIND_FLAG>(
+			D3D11_BIND_VERTEX_BUFFER | D3D11_BIND_UNORDERED_ACCESS
+		),
+		D3D11_USAGE_DYNAMIC,
+		D3D11_CPU_ACCESS_WRITE | D3D11_CPU_ACCESS_READ
+	);
+}
+
+void *CD3D11CPUSimData::GetRenderBuffer(SimBuffer buffer) {
+	switch (buffer) {
+		case SimBuffer::Position:
+			return positions.Get();
+		default:
+			return nullptr;
+	}
+}
+
+SimFloat4 *CD3D11CPUSimData::MapBuffer(SimBuffer buffer) {
+	ID3D11Buffer *targetBuffer = nullptr;
+	switch (buffer) {
+		case SimBuffer::Position:
+			targetBuffer = positions.Get();
+			break;
+		default:
+			return nullptr;
+	}
+
+	if (!targetBuffer) {
+		return nullptr;
+	}
+
+	D3D11_MAPPED_SUBRESOURCE mappedResource;
+	DX("Failed to map buffer",
+	   deviceContext->Map(
+		   targetBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource
+	   ));
+
+	return reinterpret_cast<SimFloat4 *>(mappedResource.pData);
+}
+
+void CD3D11CPUSimData::UnmapBuffer(SimBuffer buffer) {
+	ID3D11Buffer *targetBuffer = nullptr;
+	switch (buffer) {
+		case SimBuffer::Position:
+			targetBuffer = positions.Get();
+			break;
+		default:
+			return;
+	}
+
+	if (!targetBuffer) {
+		return;
+	}
+
+	deviceContext->Unmap(targetBuffer, 0);
+}
