cdc1414bed80ceb5aa07f98834e8fb1437de02b8
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-10-29 18:54:51 -0700

    Adding texture creation methods to IRenderContext.h

--- modules/gelly-fluid-renderer/include/fluidrender/IRenderContext.h
+++ modules/gelly-fluid-renderer/include/fluidrender/IRenderContext.h
@@ -3,11 +3,22 @@
 
 #include <GellyInterface.h>
 
+#include <cstdint>
+
+#include "IManagedTexture.h"
+
 // Distinction between handles and resources since this context will abstract
 // away rendering resources.
 enum class RenderAPIResource {
 	D3D11Device,
 	D3D11DeviceContext,
+};
+
+// This should only be used to verify invariants.
+// All concrete classes are expected to raise logic errors if they are
+// connected to an incompatible context.
+enum class ContextRenderAPI {
+	D3D11,
 };
 
 gelly_interface IRenderContext {
@@ -17,6 +28,30 @@ public:
 	virtual ~IRenderContext() = 0;
 
 	virtual void *GetRenderAPIResource(RenderAPIResource resource) = 0;
+	virtual ContextRenderAPI GetRenderAPI() = 0;
+
+	/**
+	 * Will throw an exception if the texture already exists!
+	 * @param name
+	 * @param desc
+	 * @return
+	 */
+	virtual IManagedTexture *CreateTexture(
+		const char *name, const GellyTextureDesc &desc
+	) = 0;
+
+	/**
+	 * Will throw an exception if the texture does not exist!
+	 * @param name
+	 * @return
+	 */
+	virtual void DestroyTexture(IManagedTexture * texture) = 0;
+
+	/**
+	 * Sets the dimensions of the context, which in turn sets the dimensions of
+	 * any fluid renderer using this context.
+	 */
+	virtual void SetDimensions(uint16_t width, uint16_t height) = 0;
 };
 
 #endif	// GELLY_IRENDERCONTEXT_H
