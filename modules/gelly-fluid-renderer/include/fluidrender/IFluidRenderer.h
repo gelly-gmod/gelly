c20e95539d339d248dffcc6267a629da5421618a
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-11-01 19:31:54 -0700

    Start working on IFluidRenderer's interface design

---
+++ modules/gelly-fluid-renderer/include/fluidrender/IFluidRenderer.h
@@ -0,0 +1,11 @@
+#ifndef GELLY_IFLUIDRENDERER_H
+#define GELLY_IFLUIDRENDERER_H
+
+#include <GellyInterface.h>
+
+gelly_interface IFluidRenderer {
+public:
+	virtual ~IFluidRenderer() = default;
+};
+
+#endif	// GELLY_IFLUIDRENDERER_H
