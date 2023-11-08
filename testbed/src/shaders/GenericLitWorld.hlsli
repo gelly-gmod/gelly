0cd8426825edf2149fb9867236ac9a25779cd94a
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-11-07 17:24:46 -0800

    Finish up huge overhaul of shader compilation and asset copying

--- testbed/src/shaders/GenericLitWorld.hlsli
+++ testbed/src/shaders/GenericLitWorld.hlsli
@@ -3,16 +3,16 @@ Basic dot-shading material for rendering the world
 */
 
 struct VS_OUTPUT {
-    float3 Pos : SV_POSITION;
+    float4 Pos : SV_POSITION;
     float3 Normal : NORMAL;
     float4 Color : COLOR;
-}
+};
 
 struct VS_INPUT {
     float3 Pos : POSITION;
     float3 Normal : NORMAL;
-}
+};
 
 struct PS_OUTPUT {
     float4 Color : SV_TARGET;
-}
+};
--- testbed/src/shaders/GenericLitWorld.vs50.hlsl
+++ testbed/src/shaders/GenericLitWorld.vs50.hlsl
@@ -5,9 +5,9 @@ VS_OUTPUT main(VS_INPUT input) {
     float4x4 mvp = model * view * projection;
 
     VS_OUTPUT output;
-    output.Pos = mul(float4(input.Pos, 1.0f), mvp).xyz;
+    output.Pos = mul(float4(input.Pos, 1.0f), mvp);
     output.Normal = input.Normal;
-    output.Color = float3(0.8, 0.8, 0.8);
+    output.Color = float4(0.8, 0.8, 0.8, 1);
 
     return output;
 }
