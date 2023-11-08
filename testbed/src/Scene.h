24bfab46b5fe73ca8e456e37da268efb7cec6b76
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-11-07 20:34:06 -0800

    Add Scene header to prepare for scene loading

---
+++ testbed/src/Scene.h
@@ -0,0 +1,27 @@
+#ifndef GELLY_SCENE_H
+#define GELLY_SCENE_H
+
+/**
+ * Since testbed is not a game engine, most of our stuff is static, and the
+ * "world" of the game is analogous to the "scene" of the testbed. This is where
+ * we load completely static world data from a gltf file.
+ */
+
+namespace testbed {
+struct SceneMetadata {
+	const char *filepath;
+};
+
+void LoadScene(const SceneMetadata &metadata);
+/**
+ * Pushes a render list to the renderer, all of the actual render data
+ * construction is done as soon as LoadScene is called.
+ *
+ * @note Call during a frame! This does not start/end a frame.
+ */
+void RenderScene();
+SceneMetadata GetCurrentSceneMetadata();
+
+}  // namespace testbed
+
+#endif	// GELLY_SCENE_H
