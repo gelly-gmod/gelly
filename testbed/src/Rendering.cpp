6739f19472482a334ffee3bbb47865721f5b6171
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-11-11 19:28:33 -0800

    Simplify float array declaration

--- testbed/src/Rendering.cpp
+++ testbed/src/Rendering.cpp
@@ -332,7 +332,7 @@ void testbed::StartFrame() {
 
 	CreateImGUIElements();
 
-	float color[4] = {1.0f, 0.0f, 0.0f, 1.0f};
+	const auto color[4] = {1.0f, 0.0f, 0.0f, 1.0f};
 	deviceContext->ClearRenderTargetView(backbufferRTV, color);
 	// Clear depth stencil
 	deviceContext->ClearDepthStencilView(
