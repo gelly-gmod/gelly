921230cec8bbe868ac0bf6a9d10c73a1cea1946a
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-12-30 12:20:24 -0800

    Remove old coupling forces

--- packages/gelly/modules/gelly-fluid-sim/src/fluidsim/CFlexSImScene.cpp
+++ packages/gelly/modules/gelly-fluid-sim/src/fluidsim/CFlexSImScene.cpp
@@ -201,7 +201,7 @@ void CFlexSimScene::Update() {
 			positions[valueIndex].x = object.second.position[0];
 			positions[valueIndex].y = object.second.position[1];
 			positions[valueIndex].z = object.second.position[2];
-			positions[valueIndex].w = 2.f;
+			positions[valueIndex].w = 1.f;
 
 			rotations[valueIndex].x = object.second.rotation[0];
 			rotations[valueIndex].y = object.second.rotation[1];
