7a8568fe2494993dd35b256489cdea282bab8b14
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2024-02-04 18:55:45 -0800

    Fix gelly-gmod's signatures

--- packages/gelly-gmod/src/source/Signatures.h
+++ packages/gelly-gmod/src/source/Signatures.h
@@ -5,12 +5,12 @@ namespace sigs {
 /**
  * \brief GMod version when the signatures were generated.
  */
-constexpr int VERSION = 240119;
+constexpr int VERSION = 240202;
 constexpr const char *const CMaterialSystem_GetLocalCubemap =
 	// Note: Only redo this sig if something huge happens--those wildcards are
 	// really important because they mask out the relative LEA offsets which
 	// pretty much makes it update-proof.
-	"40 53 48 83 ec 20 48 8b d9 48 8d 0d 04 2b 0c 00 ff 15 ?? ?? 08 00 48 8b "
+	"40 53 48 83 ec 20 48 8b d9 48 8d 0d f4 2a 0c 00 ff 15 ?? ?? 08 00 48 8b "
 	"c8 48 85 c0 75 07 48 8d 8b f0 24 00 00";
 constexpr const char *const CShaderAPIDX8_GetD3DTexture =
 	"48 85 d2 75 03 33 c0 c3 80 7a 1a 01 48 8b 42 58 74 08 0f b6 4a 1b 48 8b "
