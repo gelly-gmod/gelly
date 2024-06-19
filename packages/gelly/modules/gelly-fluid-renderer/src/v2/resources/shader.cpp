8e00c06f49d9304fe0cfef19fccf0468dccbed85
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2024-06-19 13:37:19 -0700

    Add shaders

---
+++ packages/gelly/modules/gelly-fluid-renderer/src/v2/resources/shader.cpp
@@ -0,0 +1,80 @@
+#include "shader.h"
+
+#include <helpers/throw-informative-exception.h>
+
+#include <stdexcept>
+
+namespace gelly {
+namespace renderer {
+template <typename ShaderType>
+Shader<ShaderType>::Shader(const ShaderCreateInfo &createInfo)
+	: createInfo(createInfo) {
+	if constexpr (is_vertex_shader<ShaderType>) {
+		shader = CreateVertexShader();
+	} else if constexpr (is_pixel_shader<ShaderType>) {
+		shader = CreatePixelShader();
+	} else if constexpr (is_geometry_shader<ShaderType>) {
+		shader = CreateGeometryShader();
+	}
+}
+
+template <typename ShaderType>
+auto Shader<ShaderType>::GetRawShader() -> ComPtr<ShaderType> {
+	return shader;
+}
+
+template <typename ShaderType>
+std::enable_if_t<is_vertex_shader<ShaderType>, auto>
+Shader<ShaderType>::CreateVertexShader() -> ComPtr<ID3D11VertexShader> {
+	ComPtr<ID3D11VertexShader> vertexShader;
+	const auto result = createInfo.device->GetRawDevice()->CreateVertexShader(
+		createInfo.shaderBlob,
+		createInfo.shaderBlobSize,
+		nullptr,
+		vertexShader.GetAddressOf()
+	);
+
+	GELLY_RENDERER_THROW_ON_FAIL(
+		result, std::invalid_argument, "Failed to create vertex shader"
+	);
+
+	return vertexShader;
+}
+
+template <typename ShaderType>
+std::enable_if_t<is_pixel_shader<ShaderType>, auto>
+Shader<ShaderType>::CreatePixelShader() -> ComPtr<ID3D11PixelShader> {
+	ComPtr<ID3D11PixelShader> pixelShader;
+	const auto result = createInfo.device->GetRawDevice()->CreatePixelShader(
+		createInfo.shaderBlob,
+		createInfo.shaderBlobSize,
+		nullptr,
+		pixelShader.GetAddressOf()
+	);
+
+	GELLY_RENDERER_THROW_ON_FAIL(
+		result, std::invalid_argument, "Failed to create pixel shader"
+	);
+
+	return pixelShader;
+}
+
+template <typename ShaderType>
+std::enable_if_t<is_geometry_shader<ShaderType>, auto>
+Shader<ShaderType>::CreateGeometryShader() -> ComPtr<ID3D11GeometryShader> {
+	ComPtr<ID3D11GeometryShader> geometryShader;
+	const auto result = createInfo.device->GetRawDevice()->CreateGeometryShader(
+		createInfo.shaderBlob,
+		createInfo.shaderBlobSize,
+		nullptr,
+		geometryShader.GetAddressOf()
+	);
+
+	GELLY_RENDERER_THROW_ON_FAIL(
+		result, std::invalid_argument, "Failed to create geometry shader"
+	);
+
+	return geometryShader;
+}
+}  // namespace renderer
+}  // namespace gelly
---
+++ packages/gelly/modules/gelly-fluid-renderer/src/v2/resources/shader.h
@@ -0,0 +1,66 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <d3d11.h>
+#include <helpers/comptr.h>
+
+#include <memory>
+#include <type_traits>
+
+#include "device.h"
+
+namespace gelly {
+namespace renderer {
+
+template <typename ShaderType>
+constexpr bool is_vertex_shader =
+	std::is_base_of_v<ID3D11VertexShader, ShaderType>;
+template <typename ShaderType>
+constexpr bool is_pixel_shader =
+	std::is_base_of_v<ID3D11PixelShader, ShaderType>;
+template <typename ShaderType>
+constexpr bool is_geometry_shader =
+	std::is_base_of_v<ID3D11GeometryShader, ShaderType>;
+
+template <typename ShaderType>
+class Shader {
+	static_assert(
+		is_vertex_shader<ShaderType> || is_pixel_shader<ShaderType> ||
+			is_geometry_shader<ShaderType>,
+		"ShaderType must be a D3D11 shader type"
+	);
+
+public:
+	struct ShaderCreateInfo {
+		const std::shared_ptr<Device> device;
+		void *shaderBlob;
+		unsigned int shaderBlobSize;
+	};
+
+	Shader(const ShaderCreateInfo &createInfo);
+	~Shader() = default;
+
+	auto GetRawShader() -> ComPtr<ShaderType>;
+
+private:
+	ShaderCreateInfo createInfo;
+	ComPtr<ShaderType> shader;
+
+	std::enable_if_t<is_vertex_shader<ShaderType>, auto> CreateVertexShader()
+		-> ComPtr<ID3D11VertexShader>;
+
+	std::enable_if_t<is_pixel_shader<ShaderType>, auto> CreatePixelShader()
+		-> ComPtr<ID3D11PixelShader>;
+
+	std::enable_if_t<is_geometry_shader<ShaderType>, auto> CreateGeometryShader(
+	) -> ComPtr<ID3D11GeometryShader>;
+};
+
+using PixelShader = Shader<ID3D11PixelShader>;
+using VertexShader = Shader<ID3D11VertexShader>;
+using GeometryShader = Shader<ID3D11GeometryShader>;
+
+}  // namespace renderer
+}  // namespace gelly
+
+#endif	// SHADER_H
