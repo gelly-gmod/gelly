684d4b2b687207583cd8b387af577e50400a3d58
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2024-06-20 11:33:13 -0700

    Add primary bind parsing

--- packages/gelly/modules/gelly-fluid-renderer/src/v2/helpers/parse-bind-flags.h
+++ packages/gelly/modules/gelly-fluid-renderer/src/v2/helpers/parse-bind-flags.h
@@ -3,6 +3,8 @@
 
 #include <d3d11.h>
 
+#include <optional>
+
 namespace gelly::renderer::util {
 
 struct ParsedBindFlags {
@@ -11,12 +13,34 @@ struct ParsedBindFlags {
 	bool isUAVRequired;
 };
 
+enum class PrimaryBindType { RTV, SRV, UAV };
+
 inline auto ParseBindFlags(const UINT &bindFlags) -> ParsedBindFlags {
 	return {
 		.isSRVRequired = (bindFlags & D3D11_BIND_SHADER_RESOURCE) != 0,
 		.isRTVRequired = (bindFlags & D3D11_BIND_RENDER_TARGET) != 0,
 		.isUAVRequired = (bindFlags & D3D11_BIND_UNORDERED_ACCESS) != 0
 	};
+}
+
+inline auto FindPrimaryBindType(const UINT &bindFlags)
+	-> std::optional<PrimaryBindType> {
+	// this is a non-primary bind type if it has more than one bit set
+	if (__popcnt(bindFlags) != 1) {
+		return std::nullopt;
+	}
+
+	if (D3D11_BIND_RENDER_TARGET & bindFlags) {
+		return PrimaryBindType::RTV;
+	}
+
+	if (D3D11_BIND_SHADER_RESOURCE & bindFlags) {
+		return PrimaryBindType::SRV;
+	}
+
+	if (D3D11_BIND_UNORDERED_ACCESS & bindFlags) {
+		return PrimaryBindType::UAV;
+	}
 }
 }  // namespace gelly::renderer::util
 
