a680c96bc921941b780e45167a67d8b163fc1ef7
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2024-06-28 17:41:30 -0700

    Adjust threshold ratio

--- packages/gelly-gmod/src/composite/PipelineConfig.h
+++ packages/gelly-gmod/src/composite/PipelineConfig.h
@@ -6,7 +6,7 @@
  */
 struct PipelineConfig {
 	float particleRadius;
-	float thresholdRatio = 0.144f;
+	float thresholdRatio = 0.35f;
 
 	float diffuseMotionBlur = 25.f;
 	float diffuseScale = 0.23f;
