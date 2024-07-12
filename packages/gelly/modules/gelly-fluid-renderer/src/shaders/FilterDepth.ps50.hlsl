452f01b05b999106eddb178fde784d7f0d490ed3
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2024-07-12 15:13:03 -0700

    Clean shader code up

--- packages/gelly/modules/gelly-fluid-renderer/src/shaders/FilterDepth.ps50.hlsl
+++ packages/gelly/modules/gelly-fluid-renderer/src/shaders/FilterDepth.ps50.hlsl
@@ -13,7 +13,7 @@ static const float INVALID_EYE_DEPTH_EPSILON = 0.001f;
 static const float RETAINMENT_PERCENTAGE = 0.5f; // retain 50% of last generation's normal
 
 struct PS_OUTPUT {
-    float4 Color : SV_Target0;
+    float4 FilteredNormal : SV_Target0;
 };
 
 float FetchEyeDepth(float2 pixel) {
@@ -22,25 +22,38 @@ float FetchEyeDepth(float2 pixel) {
 	return sign(eyeDepth) == -1.f ? eyeDepth : -eyeDepth;
 }
 
-float FetchProjDepth(float2 pixel) {
-    return InputDepth.Load(int3(pixel, 0)).r;
-}
-
 float3 FetchNormal(float2 pixel) {
     return InputNormal.SampleLevel(InputNormalSampler, pixel / float2(g_ViewportWidth, g_ViewportHeight), 0).xyz;
+}
+
+inline bool IsDepthInvalid(float depth) {
+	return depth >= INVALID_EYE_DEPTH_EPSILON;
+}
+
+inline bool IsNormalAllZero(float3 normal) {
+	return dot(normal, normal) == 0.f;
+}
+
+inline bool IsNormalAllOne(float3 normal) {
+	return dot(normal, normal) == 3.f;
 }
 
 float3 CreateIsosurfaceNormals(float2 tex) {
     // We compute the normal of the isosurface at the given pixel by sampling the depth and normal textures
     float2 centerPixel = tex * float2(g_ViewportWidth, g_ViewportHeight);
+
     /*
+	3x3 isosurface normal kernel
     [ 0 ] [ 1 ] [ 2 ]
     [ 3 ] [ 4 ] [ 5 ]
     [ 6 ] [ 7 ] [ 8 ]
 
     We, at filter-time, generate a kernel which remaps the depth values from the maximum being 1/9 to zero.
     This allows us to sample the depth values in a way that is more robust to noise, and properly filters out discontinuities.
+	We also retain 50% of the last generation's normal to prevent losing features and valid data which may have too small of a footprint to
+	be considered otherwise.
     */
+
     float3 normalTaps[9] = {
         FetchNormal(centerPixel + float2(-1, -1)),
         FetchNormal(centerPixel + float2( 0, -1)),
@@ -68,35 +81,32 @@ float3 CreateIsosurfaceNormals(float2 tex) {
     [unroll]
     for (int i = 0; i < 9; i++) {
         // our pipeline initializes every invalid depth to D3D11_FLOAT32_MAX so this is how we can check for invalid depth
-        if (kernel[i] > INVALID_EYE_DEPTH_EPSILON) {
+        if (IsDepthInvalid(kernel[i])) {
             kernel[i] = kernel[4]; // center pixel
-            // we'll also do symmetric balancing for the kernel
+            // technically, this isn't symmetric, but it's close enough and makes for a nicer visual result. Plus, it's faster.
             kernel[9 - i] = kernel[4]; // center pixel
         }
 
-        // 0 * 0 = 0, so we can use this to check if the normal is invalid
-        // and (1*1+1*1+1*1) = 3, so we can use this to check if the normal is invalid too
-        if (dot(normalTaps[i], normalTaps[i]) == 0.f || dot(normalTaps[i], normalTaps[i]) == 3.f) {
+        if (IsNormalAllZero(normalTaps[i]) || IsNormalAllOne(normalTaps[i])) {
             kernel[i] = 0.f; // discard this kernel value if the normal is invalid
         }
     }
 
     float centerKernel = kernel[4];
     [unroll]
-    for (int i = 0; i < 9; i++) {
-        if (kernel[i] == 0.f) {
+    for (int j = 0; j < 9; j++) { // fyi: different variable name to avoid shadowing since hlsl is unrolling every loop
+        if (kernel[j] == 0.f) {
             continue;
         }
 
-        kernel[i] /= centerKernel; // normalize the kernel
-        // We want to preserve edges and curves as much as possible, so we will nudge the kernel values depending on the normals
-        kernel[i] *= 1 / 9.f;
+        kernel[j] /= centerKernel; // normalize the kernel
+        kernel[j] *= 1 / 9.f;
     }
 
     float3 normal = float3(0, 0, 0);
     [unroll]
-    for (int i = 0; i < 9; i++) {
-        normal += normalTaps[i] * kernel[i];
+    for (int k = 0; k < 9; k++) {
+        normal += normalTaps[k] * kernel[k];
     }
 
     normal = normalize(normal);
@@ -107,14 +117,15 @@ float3 CreateIsosurfaceNormals(float2 tex) {
 
 PS_OUTPUT main(VS_OUTPUT input) {
     PS_OUTPUT output = (PS_OUTPUT)0;
-    float2 original = InputDepth.SampleLevel(InputDepthSampler, input.Tex, 0);
-    if (original.r >= 1.f) {
+    float projDepth = InputDepth.SampleLevel(InputDepthSampler, input.Tex, 0).r;
+
+    if (projDepth >= 1.f) {
         discard;
     }
 
 	// there's really no point in filtering the depth if it's underwater
-	if (IsProjDepthUnderwater(original)) {
-		output.Color = float4(original, 0.f, 1.f);
+	if (IsProjDepthUnderwater(projDepth)) {
+		output.FilteredNormal = float4(0, 0, 0, 1);
 		return output;
 	}
 
@@ -122,10 +133,10 @@ PS_OUTPUT main(VS_OUTPUT input) {
     if (isnan(normal.x) || isnan(normal.y) || isnan(normal.z)) {
         // return the original normal if the new one is invalid
         normal = FetchNormal(input.Tex);
-        output.Color = float4(normal, 1.f);
+        output.FilteredNormal = float4(normal, 1.f);
         return output;
     }
 
-    output.Color = float4(normal, 1.f);
+    output.FilteredNormal = float4(normal, 1.f);
     return output;
 }
