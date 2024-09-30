76636c16f9a8230fd9602af7672c00c33277a949
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2024-09-29 18:13:03 -0700

    Add color overrides to disc emitters

--- packages/addon/lua/entities/gelly_disc_emitter.lua
+++ packages/addon/lua/entities/gelly_disc_emitter.lua
@@ -10,23 +10,26 @@ ENT.AdminOnly = false
 ENT.Editable = true
 
 --- Setups the given list of network vars
----@param vars {name: string, type: string, min: number?, max: number?}[]
+--- To get a color picker, use type "Vector" and editType "VectorColor"
+---@param vars {name: string, type: string, editType: string?, min: number?, max: number?}[]
 function ENT:SetupEditableNetworkVars(vars)
 	local perTypeCounters = {}
 	for index, var in ipairs(vars) do
 		perTypeCounters[var.type] = perTypeCounters[var.type] or 0
 		local counter = perTypeCounters[var.type]
 		self:NetworkVar(var.type, counter, var.name,
-			{ KeyName = var.name:lower(), Edit = { type = var.type, order = index + 1, min = var.min, max = var.max } })
+			{ KeyName = var.name:lower(), Edit = { type = var.editType and var.editType or var.type, order = index + 1, min = var.min, max = var.max } })
 		perTypeCounters[var.type] = counter + 1
 	end
 end
 
 function ENT:SetupDataTables()
 	self:SetupEditableNetworkVars({
-		{ name = "Density", type = "Float", min = 50, max = 1000 },
-		{ name = "Speed",   type = "Float", min = 1,  max = 255 },
-		{ name = "Enabled", type = "Bool" }
+		{ name = "Density",       type = "Float",  min = 50,                max = 1000 },
+		{ name = "Speed",         type = "Float",  min = 1,                 max = 255 },
+		{ name = "ColorOverride", type = "Vector", editType = "VectorColor" },
+		{ name = "ColorScale",    type = "Float",  min = 1,                 max = 4 },
+		{ name = "Enabled",       type = "Bool" }
 	})
 end
 
@@ -53,7 +56,23 @@ function ENT:Initialize()
 		end
 
 		self:InitializeDefaultSettings()
+		return
 	end
+
+	self.PresetMaterial = gellyx.presets.copyPresetMaterial(gellyx.presets.getActivePreset().Name)
+end
+
+function ENT:GetAbsorptionOverride()
+	local absorptionR = 1 - self:GetColorOverride().x
+	local absorptionG = 1 - self:GetColorOverride().y
+	local absorptionB = 1 - self:GetColorOverride().z
+
+	return Vector(absorptionR, absorptionG, absorptionB) * self:GetColorScale()
+end
+
+function ENT:IsColorOverrided()
+	local override = self:GetColorOverride()
+	return override.x ~= 0 or override.y ~= 0 or override.z ~= 0
 end
 
 function ENT:Think()
@@ -61,14 +80,20 @@ function ENT:Think()
 		return
 	end
 
+	if self:IsColorOverrided() then
+		self.PresetMaterial.Absorption = self:GetAbsorptionOverride()
+	end
+
 	if self:GetEnabled() then
 		local transform = self:GetWorldTransformMatrix()
+
 		gellyx.emitters.Cube({
 			center = Vector(0, 0, 50),
 			velocity = Vector(0, 0, self:GetSpeed()),
 			bounds = Vector(2.3 * gellyx.presets.getEffectiveRadius(), 1.6 * gellyx.presets.getEffectiveRadius(), 80),
 			density = self:GetDensity(),
 			transform = transform,
+			material = self:IsColorOverrided() and self.PresetMaterial or nil
 		})
 	end
 
