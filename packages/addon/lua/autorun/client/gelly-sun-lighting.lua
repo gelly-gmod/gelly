b18db14e930d6c0ea7cd86327420629c16e1e8ea
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2024-10-01 14:30:32 -0700

    Add a CheckBox component to the UI, allow users to disable sun lighting

--- packages/addon/lua/autorun/client/gelly-sun-lighting.lua
+++ packages/addon/lua/autorun/client/gelly-sun-lighting.lua
@@ -2,13 +2,8 @@ local SUN_VISIBILITY_UPDATE_INTERVAL_SECONDS = 0.1
 local logging = include("gelly/logging.lua")
 
 hook.Add("GellyLoaded", "gelly.initialize-sun", function()
-	local sunVisible = gellyx.settings.get("sun_visible"):GetBool()
-	gelly.SetSunEnabled(sunVisible)
-
-	if not sunVisible then
-		logging.warn("Sun is disabled, not initializing")
-		return
-	end
+	local sunEnabled = gellyx.settings.get("sun_visible"):GetBool()
+	gelly.SetSunEnabled(sunEnabled)
 
 	local sunInfo = util.GetSunInfo()
 	if not sunInfo then
@@ -22,6 +17,11 @@ hook.Add("GellyLoaded", "gelly.initialize-sun", function()
 	gelly.SetSunDirection(sunDirection)
 
 	timer.Create("gelly.update-sun-visibility", SUN_VISIBILITY_UPDATE_INTERVAL_SECONDS, 0, function()
+		if not gellyx.settings.get("sun_visible"):GetBool() then
+			gelly.SetSunEnabled(false)
+			return
+		end
+
 		local eyePos = LocalPlayer():EyePos()
 		local sunPos = sunDirection * 65535
 
