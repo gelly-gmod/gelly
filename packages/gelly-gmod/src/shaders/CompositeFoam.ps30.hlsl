aab71841551afdfee5a7e72da141b852acfe187f
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2024-10-25 13:01:02 -0700

    Remove unused constants in the foam shader

--- packages/gelly-gmod/src/shaders/CompositeFoam.ps30.hlsl
+++ packages/gelly-gmod/src/shaders/CompositeFoam.ps30.hlsl
@@ -4,23 +4,7 @@
 
 sampler2D foamTex : register(s0);
 sampler2D depthTex : register(s1);
-
-float4 eyePos : register(c0);
-float4 refractAndCubemapStrength : register(c1);
-
-struct CompositeLight {
-    float4 LightInfo;
-    float4 Position;
-    float4 Enabled;
-};
-
-CompositeLight lights[2] : register(c2); // next reg that can be used is c8 (2 + 6)
-float4 aspectRatio : register(c8);
 float4 ambientCube[6] : register(c9);
-float4x4 viewProjMatrix : register(c17);
-float4 sunDir : register(c21);
-float4 lightScaling : register(c22);
-float4x4 invViewProjMatrix : register(c23);
 
 struct PS_OUTPUT {
     float4 Color : SV_TARGET0;
