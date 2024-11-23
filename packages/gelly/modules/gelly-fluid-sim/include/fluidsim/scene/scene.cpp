6322033aef53d58d73a3e36cb8f66f383ab6eafd
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2024-11-23 10:08:35 -0800

    Add missing method

--- packages/gelly/modules/gelly-fluid-sim/include/fluidsim/scene/scene.cpp
+++ packages/gelly/modules/gelly-fluid-sim/include/fluidsim/scene/scene.cpp
@@ -17,6 +17,10 @@ std::shared_ptr<ShapeHandler> Scene::GetShapeHandler() const {
 	return shapeHandler;
 }
 
+std::shared_ptr<ForcefieldHandler> Scene::GetForcefieldHandler() const {
+	return forcefieldHandler;
+}
+
 void Scene::Update() {
 	for (const auto &handler : handlers) {
 		handler->Update();
