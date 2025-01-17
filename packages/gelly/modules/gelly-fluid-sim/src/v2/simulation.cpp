472c8d50c356f3f20f6bebf065b509bd7f32c02d
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2025-01-16 19:20:42 -0800

    Implement main simulation

--- packages/gelly/modules/gelly-fluid-sim/src/v2/simulation.cpp
+++ packages/gelly/modules/gelly-fluid-sim/src/v2/simulation.cpp
@@ -1,7 +1,29 @@
-
 #include "simulation.h"
 
-namespace gelly {
-namespace simulation {
-} // simulation
-} // gelly
+namespace gelly::simulation {
+Simulation::Simulation(const CreateInfo &createInfo) :
+	info(createInfo),
+	library(CreateLibrary()),
+	solver(
+		{.library = library,
+		 .maxParticles = createInfo.maxParticles,
+		 .maxDiffuseParticles = createInfo.maxDiffuseParticles,
+		 .rendererBuffers = createInfo.rendererBuffers}
+	) {}
+
+Simulation::~Simulation() {
+	if (library) {
+		NvFlexShutdown(library);
+	}
+}
+
+NvFlexLibrary *Simulation::CreateLibrary() const {
+	NvFlexInitDesc desc = {};
+	desc.renderDevice = info.device;
+	desc.renderContext = info.context;
+	desc.enableExtensions = false;
+	desc.runOnRenderContext = true;
+
+	return NvFlexInit(NV_FLEX_VERSION, ErrorHandler, &desc);
+}
+}  // namespace gelly::simulation
--- packages/gelly/modules/gelly-fluid-sim/src/v2/simulation.h
+++ packages/gelly/modules/gelly-fluid-sim/src/v2/simulation.h
@@ -2,6 +2,7 @@
 #include <d3d11.h>
 
 #include "NvFlex.h"
+#include "solver.h"
 
 namespace gelly::simulation {
 
@@ -13,14 +14,27 @@ public:
 
 		int maxParticles;
 		int maxDiffuseParticles;
+
+		OutputD3DBuffers rendererBuffers;
 	};
 
 	Simulation(const CreateInfo &createInfo);
 	~Simulation();
 
+	Solver &GetSolver() { return solver; }
+
 private:
 	CreateInfo info;
 	NvFlexLibrary *library;
+	Solver solver;
+
+	NvFlexLibrary *CreateLibrary() const;
+	static void ErrorHandler(
+		NvFlexErrorSeverity severity,
+		const char *msg,
+		const char *file,
+		int line
+	);
 };
 
 }  // namespace gelly::simulation
