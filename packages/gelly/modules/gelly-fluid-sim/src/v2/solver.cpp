bb1a08c4a92e7c85c3d67cb90a41b7f6ba366bb9
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2025-01-16 18:17:09 -0800

    Add more features to new solver

--- packages/gelly/modules/gelly-fluid-sim/src/v2/solver.cpp
+++ packages/gelly/modules/gelly-fluid-sim/src/v2/solver.cpp
@@ -15,6 +15,117 @@ Solver::Solver(const CreateInfo &createInfo) :
 
 Solver::~Solver() { NvFlexDestroySolver(solver); }
 
+void Solver::Tick(float dt) {
+	if (newActiveParticleCount != activeParticleCount) {
+		activeParticleCount = newActiveParticleCount;
+	}
+
+	dt *= timeStepMultiplier;
+
+	NvFlexCopyDesc copyDesc = {};
+	copyDesc.srcOffset = 0;
+	copyDesc.dstOffset = 0;
+	copyDesc.elementCount = info.maxParticles;
+
+	NvFlexSetParams(solver, &params);
+	NvFlexSetActiveCount(solver, activeParticleCount);
+	NvFlexUpdateSolver(solver, dt, substeps, false);
+
+	NvFlexGetSmoothParticles(
+		solver, *outputBuffers.smoothedPositions, &copyDesc
+	);
+
+	NvFlexGetAnisotropy(
+		solver,
+		*outputBuffers.anisotropyQ1,
+		*outputBuffers.anisotropyQ2,
+		*outputBuffers.anisotropyQ3,
+		&copyDesc
+	);
+}
+
+void Solver::AddParticles(const ParticleBatch &particles) {
+	const auto startCount = newActiveParticleCount;
+	const auto endCount = startCount + particles.size();
+
+	if (endCount >= info.maxParticles) {
+		return;	 // Drop particles
+	}
+
+	auto positions = buffers.positions.Map();
+	auto velocities = buffers.velocities.Map();
+	auto phases = buffers.phases.Map();
+	auto actives = buffers.actives.Map();
+
+	for (int index = 0; index < particles.size(); index++) {
+		const auto &particle = particles[index];
+		positions[index] = {
+			particle.position.x, particle.position.y, particle.position.z, 1.0f
+		};
+
+		velocities[index] = particle.velocity;
+
+		phases[index] =
+			NvFlexMakePhase(0, eNvFlexPhaseSelfCollide | eNvFlexPhaseFluid);
+
+		actives[index] = index;
+	}
+
+	newActiveParticleCount = endCount;
+
+	buffers.positions.Unmap();
+	buffers.velocities.Unmap();
+	buffers.phases.Unmap();
+	buffers.actives.Unmap();
+
+	NvFlexCopyDesc copyDesc = {};
+	copyDesc.srcOffset = 0;
+	copyDesc.dstOffset = 0;
+	copyDesc.elementCount = newActiveParticleCount;
+
+	NvFlexSetParticles(solver, *buffers.positions, &copyDesc);
+	NvFlexSetVelocities(solver, *buffers.velocities, &copyDesc);
+	NvFlexSetPhases(solver, *buffers.phases, &copyDesc);
+	NvFlexSetActive(solver, *buffers.actives, &copyDesc);
+}
+
+void Solver::Reset() { newActiveParticleCount = 0; }
+
+#define UPDATE_FLEX_PARAM(name, paramName)    \
+	if (info.paramName.has_value()) {         \
+		params.name = info.paramName.value(); \
+	}
+
+void Solver::Update(const UpdateSolverInfo &info) {
+	UPDATE_FLEX_PARAM(radius, radius);
+	UPDATE_FLEX_PARAM(viscosity, viscosity);
+	UPDATE_FLEX_PARAM(cohesion, cohesion);
+	if (info.surfaceTension.has_value()) {
+		params.surfaceTension = *info.surfaceTension / powf(params.radius, 5.f);
+	}
+	UPDATE_FLEX_PARAM(vorticityConfinement, vorticityConfinement);
+	UPDATE_FLEX_PARAM(adhesion, adhesion);
+	UPDATE_FLEX_PARAM(dynamicFriction, dynamicFriction);
+	if (info.restDistanceRatio.has_value()) {
+		params.fluidRestDistance = *info.restDistanceRatio * params.radius;
+	}
+	UPDATE_FLEX_PARAM(diffuseBallistic, diffuseBallisticCount);
+	UPDATE_FLEX_PARAM(diffuseThreshold, diffuseKineticThreshold);
+	UPDATE_FLEX_PARAM(diffuseBuoyancy, diffuseBuoyancy);
+	UPDATE_FLEX_PARAM(diffuseDrag, diffuseDrag);
+	UPDATE_FLEX_PARAM(diffuseLifetime, diffuseLifetime);
+	UPDATE_FLEX_PARAM(numIterations, iterations);
+	UPDATE_FLEX_PARAM(relaxationFactor, relaxationFactor);
+	UPDATE_FLEX_PARAM(collisionDistance, collisionDistance);
+	UPDATE_FLEX_PARAM(gravity[2], gravity);
+
+	if (info.substeps.has_value()) {
+		substeps = *info.substeps;
+	}
+}
+
+int Solver::GetActiveParticleCount() const { return activeParticleCount; }
+
 NvFlexSolver *Solver::CreateSolver() const {
 	NvFlexSolverDesc desc = {};
 	NvFlexSetSolverDescDefaults(&desc);
--- packages/gelly/modules/gelly-fluid-sim/src/v2/solver.h
+++ packages/gelly/modules/gelly-fluid-sim/src/v2/solver.h
@@ -1,18 +1,21 @@
 #pragma once
 #include <DirectXMath.h>
 
+#include <optional>
 #include <vector>
 
 #include "NvFlex.h"
 #include "helpers/flex-buffer.h"
 #include "helpers/flex-gpu-buffer.h"
 
+#define OPTIONAL_PARAM(name, type) std::optional<type> name = std::nullopt;
+
 namespace gelly::simulation {
 using namespace helpers;
 
 struct SolverBufferSet {
 	FlexBuffer<DirectX::XMFLOAT4> positions;
-	FlexBuffer<DirectX::XMFLOAT4> velocities;
+	FlexBuffer<DirectX::XMFLOAT3> velocities;
 	FlexBuffer<int> phases;
 	FlexBuffer<int> actives;
 
@@ -96,6 +99,13 @@ struct OutputBuffers {
 		}) {}
 };
 
+struct ParticleUploadInfo {
+	DirectX::XMFLOAT3 position;
+	DirectX::XMFLOAT3 velocity;
+};
+
+using ParticleBatch = std::vector<ParticleUploadInfo>;
+
 class Solver {
 public:
 	struct CreateInfo {
@@ -107,16 +117,47 @@ public:
 		OutputD3DBuffers rendererBuffers;
 	};
 
+	struct UpdateSolverInfo {
+		OPTIONAL_PARAM(radius, float);
+		OPTIONAL_PARAM(viscosity, float);
+		OPTIONAL_PARAM(cohesion, float);
+		OPTIONAL_PARAM(surfaceTension, float);
+		OPTIONAL_PARAM(vorticityConfinement, float);
+		OPTIONAL_PARAM(adhesion, float);
+		OPTIONAL_PARAM(dynamicFriction, float);
+		OPTIONAL_PARAM(restDistanceRatio, float);
+		OPTIONAL_PARAM(diffuseBallisticCount, float);
+		OPTIONAL_PARAM(diffuseKineticThreshold, float);
+		OPTIONAL_PARAM(diffuseBuoyancy, float);
+		OPTIONAL_PARAM(diffuseDrag, float);
+		OPTIONAL_PARAM(diffuseLifetime, float);
+		OPTIONAL_PARAM(substeps, int);
+		OPTIONAL_PARAM(iterations, int);
+		OPTIONAL_PARAM(relaxationFactor, float);
+		OPTIONAL_PARAM(collisionDistance, float);
+		OPTIONAL_PARAM(gravity, float);
+		OPTIONAL_PARAM(enableWhitewater, bool);
+	};
+
 	Solver(const CreateInfo &createInfo);
 	~Solver();
 
 	void Tick(float dt);
+	void AddParticles(const ParticleBatch &particles);
+	void Update(const UpdateSolverInfo &info);
+	void Reset();
+
+	int GetActiveParticleCount() const;
 
 private:
 	CreateInfo info;
 	NvFlexSolver *solver;
 	NvFlexParams params;
 	float timeStepMultiplier = 1.f;
+	int activeParticleCount = 0;
+	int newActiveParticleCount = 0;
+
+	int substeps = 3;
 
 	SolverBufferSet buffers;
 	OutputBuffers outputBuffers;
